e7ba98d9cfaaefa3d7064a2480a0d41aa86937a2
Nikita Romanov <acce.ventura@icloud.com>
2026-03-08 13:36:14 +0400

    .

--- LanguageServer.Framework/Protocol/Message/WorkspaceFolders/WorkspaceFoldersChangeEvent.cs
+++ LanguageServer.Framework/Protocol/Message/WorkspaceFolders/WorkspaceFoldersChangeEvent.cs
@@ -1,14 +1,18 @@
-﻿namespace EmmyLua.LanguageServer.Framework.Protocol.Message.WorkspaceFolders;
+﻿using System.Text.Json.Serialization;
+
+namespace EmmyLua.LanguageServer.Framework.Protocol.Message.WorkspaceFolders;
 
 public class WorkspaceFoldersChangeEvent
 {
     /**
      * The array of added workspace folders
      */
+    [JsonPropertyName("added")]
     public List<Model.WorkspaceFolder> Added { get; set; } = null!;
 
     /**
      * The array of the removed workspace folders
      */
+    [JsonPropertyName("removed")]
     public List<Model.WorkspaceFolder> Removed { get; set; } = null!;
 }
